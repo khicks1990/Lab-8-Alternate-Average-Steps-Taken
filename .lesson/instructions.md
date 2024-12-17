f8f46380923d10e588cc5d90e763596970e95d9b
khicks1990 <85317318+khicks1990@users.noreply.github.com>
2024-12-17 07:44:45 +0000

    Initial commit

---
+++ .lesson/instructions.md
@@ -0,0 +1,5 @@
+# Instructions  
+
+A Personal Fitness Tracker is a wearable device that tracks your physical activity, calories burned, heart rate, sleeping patterns, etc. One common physical activity that most of these devices track is the number of steps you take each day.
+
+ The steps.txt file contains the number of steps a person has taken each day for a year. There are 365 lines in the file, and each line contains the number of steps taken during a day. (The first line is the number of steps taken on January 1, the second line is the number of steps taken on January 2, and so forth.) Write a program that reads the file, and then displays the average number of steps taken for each month. (The data is from a year that was not a leap year, so February has 28 days.)
---
+++ Main.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.io.*;
+
+/**
+ * Average Steps Taken
+ */
+
+public class Main
+{
+   // Named constants for the months
+
+
+   // Named constants for the days in the months
+
+
+   // Array of the month names
+
+
+   // Array of the days of the months
+
+
+   public static void main(String[] args) throws IOException
+   {
+
+   }
+
+   // Returns the average number of steps for the given month
+   private static double averageSteps(int[] steps, int month)
+   {
+      int days = 0;
+      double total = 0.0;
+      double average;
+
+      // Calculate the starting position in the ArrayList
+
+
+      // Go to the beginning of the specified month
+
+
+      // Calculate the average of the steps
+
+
+      // Return the average
+
+   }
+}
